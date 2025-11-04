4e39c262edf8b123fd57fcffccc3b8b11bcef7d6
Le Xuan Truong <namtruongpro1232@gmail.com>
2025-11-04 22:37:42 +0700

    hello

--- Main.java
+++ Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Worrld");
         System.out.println("conc ac");
+        System.out.println("conc ac");
+
     }
 }
